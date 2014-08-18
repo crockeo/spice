bf6b504652aef63fdccb4b15f1fcae18986eaff1
Crockeo <crockeo@gmail.com>
2014-08-17 22:22:41 -0400

    Added a 'get' preface to all of the config fields.

--- src/FRP/Spice/Config.hs
+++ src/FRP/Spice/Config.hs
@@ -8,21 +8,21 @@ import Data.Default
 -- Code --
 
 -- The window config
-data WindowConfig = WindowConfig { windowWidth      :: Int
-                                 , windowHeight     :: Int
-                                 , windowFullscreen :: Bool
-                                 , windowResizable  :: Bool
-                                 , windowTitle      :: String
+data WindowConfig = WindowConfig { getWindowWidth      :: Int
+                                 , getWindowHeight     :: Int
+                                 , getWindowFullscreen :: Bool
+                                 , getWindowResizable  :: Bool
+                                 , getWindowTitle      :: String
                                  }
   deriving (Eq, Show, Read)
 
 -- The default window config
 defaultWindowConfig :: WindowConfig
-defaultWindowConfig = WindowConfig { windowWidth      = 640
-                                   , windowHeight     = 480
-                                   , windowFullscreen = False
-                                   , windowResizable  = False
-                                   , windowTitle      = "Spice Application"
+defaultWindowConfig = WindowConfig { getWindowWidth      = 640
+                                   , getWindowHeight     = 480
+                                   , getWindowFullscreen = False
+                                   , getWindowResizable  = False
+                                   , getWindowTitle      = "Spice Application"
                                    }
 
 -- A default instance for the window
