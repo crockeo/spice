0f1cefde71a0eb7df10fab89e4041320459334ab
Crockeo <crockeo@gmail.com>
2014-08-20 11:00:28 -0400

    Added a scene and an element type.

--- src/FRP/Spice/Graphics.hs
+++ src/FRP/Spice/Graphics.hs
@@ -2,12 +2,16 @@
   This module re-exports a number of modules having to do with graphics.
 -}
 module FRP.Spice.Graphics ( module FRP.Spice.Graphics.Geometry
+                          , module FRP.Spice.Graphics.Element
                           , module FRP.Spice.Graphics.Color
+                          , module FRP.Spice.Graphics.Scene
                           , module FRP.Spice.Graphics.Utils
                           ) where
 
 -----------------------
 -- Rexported Imports --
 import FRP.Spice.Graphics.Geometry
+import FRP.Spice.Graphics.Element
 import FRP.Spice.Graphics.Color
+import FRP.Spice.Graphics.Scene
 import FRP.Spice.Graphics.Utils
---
+++ src/FRP/Spice/Graphics/Scene.hs
@@ -0,0 +1,54 @@
+module FRP.Spice.Graphics.Scene where
+
+--------------------
+-- Global Imports --
+import Control.Applicative
+import Control.Monad
+
+-------------------
+-- Local Imports --
+import FRP.Spice.Graphics.Element
+
+----------
+-- Code --
+
+{-|
+  For composing a scene out of a set of elements.
+-}
+data SceneT a = SceneT [Element] a
+
+{-|
+  The commonly used instance of SceneT
+-}
+type Scene = SceneT ()
+
+{-|
+  Functor instance to satisfy applicative instance.
+-}
+instance Functor SceneT where
+  fmap fn (SceneT elements v) = SceneT elements $ fn v
+
+{-|
+  Applicative instance to satisfy the monad instance. Not advised to use
+-}
+instance Applicative SceneT where
+  pure  = return
+  (<*>) = ap
+
+{-|
+  Used for being able to compose Scenes in a do-notation. Not very useful
+  outside of that.
+-}
+instance Monad SceneT where
+  return a = SceneT [] a
+  (SceneT elements v) >>= fn =
+    let (SceneT elements' v') = fn v in
+      SceneT (elements ++ elements') v'
+
+{-|
+  Rendering a whole @'Scene'@ (renders each @'Element'@ from first in list to last in
+  list.)
+-}
+renderScene :: Scene -> IO ()
+renderScene (SceneT elements _) =
+  forM_ elements renderElement
