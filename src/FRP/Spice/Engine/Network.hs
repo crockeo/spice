56cbbb73a6265379de45e741d1394e745d6a36d7
Cerek Hillen <crockeo@gmail.com>
2014-08-20 23:16:06 -0400

    Removed the old API.

--- src/FRP/Spice/Engine/Network.hs
+++ src/FRP/Spice/Engine/Network.hs
@@ -32,19 +32,3 @@ updateFn gameSink dt input game _ = do
 makeNetwork :: Game a => Signal Input -> Signal a -> (a -> IO ()) -> IO (Float -> IO (IO ()))
 makeNetwork inputSignal gameSignal gameSink =
   start $ transfer2 (return ()) (updateFn gameSink) inputSignal gameSignal
-
-{-
-{-|
-  Creating a network to be used with the
-  @'FRP.Spice.Engine.Driver.driveNetwork'@ function.
--}
-makeNetwork :: Game a => Signal Input -> Signal a -> (a -> IO ()) -> IO (Float -> IO (IO ()))
-makeNetwork inputSignal gameSignal gameSink =
-  start $ memo $ do
-    input <- inputSignal
-    game  <- gameSignal
-
-    return $ do
-      gameSink $ update 0.01 input game
-      renderWrapper $ render game
--}
