c437bbe196d9ee1f4caf397198ca31a1af466d81
Crockeo <crockeo@gmail.com>
2014-08-24 15:12:51 -0400

    removed the other sprite rendering functions for now.

--- src/FRP/Spice/Graphics/RenderSprite.hs
+++ src/FRP/Spice/Graphics/RenderSprite.hs
@@ -19,19 +19,3 @@ import FRP.Spice.Math
 -}
 renderSprite :: Sprite -> Vector Float -> Scene
 renderSprite sprite pos = undefined
-
-{-|
-  Rendering a @'Sprite'@, similar to @'renderSprite'@, but with the addition of
-  being able to specify the size.
--}
-renderSpriteWithSize :: Sprite -> Vector Float -> Vector Float -> Scene
-renderSpriteWithSize sprite pos size = undefined
-
-{-|
-  Rendering a @'Sprite'@, similarly to @'renderSprite'@, but allowing the
-  ability to specify the desired rotation (in the form of a @'Float'@ ranging
-  from 0-360. Anything below that range will become 360-n, anything above that
-  range will be n-360.)
--}
-renderSpriteWithRotation :: Sprite -> Vector Float -> Float -> Scene
-renderSpriteWithRotation sprite pos rot = undefined
