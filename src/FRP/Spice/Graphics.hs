13e0f88a0ab3f4d6255f61e269b7ec9409298fa2
Crockeo <crockeo@gmail.com>
2014-08-25 00:45:16 -0400

    Removed the references to Sprites in the Graphics file.

--- src/FRP/Spice/Graphics.hs
+++ src/FRP/Spice/Graphics.hs
@@ -1,9 +1,7 @@
 {-|
   This module re-exports a number of modules having to do with graphics.
 -}
-module FRP.Spice.Graphics ( module FRP.Spice.Graphics.RenderSprite
-                          , module FRP.Spice.Graphics.Geometry
-                          , module FRP.Spice.Graphics.Sprite
+module FRP.Spice.Graphics ( module FRP.Spice.Graphics.Geometry
                           , module FRP.Spice.Graphics.Color
                           , module FRP.Spice.Graphics.Scene
                           , module FRP.Spice.Graphics.Utils
@@ -11,9 +9,7 @@ module FRP.Spice.Graphics ( module FRP.Spice.Graphics.RenderSprite
 
 -----------------------
 -- Rexported Imports --
-import FRP.Spice.Graphics.RenderSprite
 import FRP.Spice.Graphics.Geometry
-import FRP.Spice.Graphics.Sprite
 import FRP.Spice.Graphics.Color
 import FRP.Spice.Graphics.Scene
 import FRP.Spice.Graphics.Utils
