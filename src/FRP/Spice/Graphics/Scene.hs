5ce5cc988897965435429a301776636daae67de1
Crockeo <crockeo@gmail.com>
2014-08-24 22:48:32 -0400

    Revert "Added some documentation to Scene functions."
    This reverts commit 997174d9aed47125c0f4645ec9019869f005ba59.

--- src/FRP/Spice/Graphics/Scene.hs
+++ src/FRP/Spice/Graphics/Scene.hs
@@ -16,16 +16,9 @@ import FRP.Spice.Utils.DoList
 -}
 type Scene = DoList [Element]
 
-{-|
-  Converting a list of @'Element'@s into a @'Scene'@ so that it may be composed
-  through do-notation.
--}
 fromElements :: [Element] -> Scene
 fromElements = fromValues
 
-{-|
-  Rendering a Scene (effectively the same as running @'renderelement'@ on a
-  list of @'Element'@s.)
--}
 renderScene :: Scene -> IO ()
-renderScene = mapM_ renderElement . values
+renderScene scene =
+  mapM_ renderElement $ values scene
