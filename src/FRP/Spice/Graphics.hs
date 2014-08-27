2627abaeb8d3092eff668e5d5c57094530513357
Cerek Hillen <crockeo@gmail.com>
2014-08-27 18:29:53 -0400

    Exporting the Sprite.

--- src/FRP/Spice/Graphics.hs
+++ src/FRP/Spice/Graphics.hs
@@ -2,6 +2,7 @@
   This module re-exports a number of modules having to do with graphics.
 -}
 module FRP.Spice.Graphics ( module FRP.Spice.Graphics.Geometry
+                          , module FRP.Spice.Graphics.Sprite
                           , module FRP.Spice.Graphics.Color
                           , module FRP.Spice.Graphics.Scene
                           , module FRP.Spice.Graphics.Utils
@@ -10,6 +11,7 @@ module FRP.Spice.Graphics ( module FRP.Spice.Graphics.Geometry
 -----------------------
 -- Rexported Imports --
 import FRP.Spice.Graphics.Geometry
+import FRP.Spice.Graphics.Sprite
 import FRP.Spice.Graphics.Color
 import FRP.Spice.Graphics.Scene
 import FRP.Spice.Graphics.Utils
