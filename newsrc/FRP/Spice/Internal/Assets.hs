dd744b273207130fea91a1677ef91efd3d7b91b4
Crockeo <crockeo@gmail.com>
2014-08-30 22:36:37 -0400

    Moved the load<asset>Asset functions to their own file.

--- newsrc/FRP/Spice/Internal/Assets.hs
+++ newsrc/FRP/Spice/Internal/Assets.hs
@@ -1,9 +1,7 @@
 {-|
   This module provides functions to load and manage the loading of assets.
 -}
-module FRP.Spice.Internal.Assets ( loadSpriteAsset
-                                 , performAssetLoads
-                                 ) where
+module FRP.Spice.Internal.Assets (performAssetLoads) where
 
 --------------------
 -- Global Imports --
@@ -71,12 +69,6 @@ loadSprite path = do
 appendSprite :: Assets -> FilePath -> Sprite -> Assets
 appendSprite assets path sprite =
   assets { sprites = Map.insert path sprite $ sprites assets }
-
-{-|
-  Creating a @'LoadAssets'@ from a call to load a sprite from a file.
--}
-loadSpriteAsset :: FilePath -> LoadAssets
-loadSpriteAsset path = DoListT [LoadSprite path] ()
 
 {-|
   Performing all of the @'LoadAsset'@ commands contained in a @'LoadAssets'@.
